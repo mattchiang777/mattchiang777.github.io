2ec3a0ea6b47b5f2ccaf21d47c002619f8f416c9
Matthew Chiang <matthewchiang@Matthews-MacBook-Pro.local>
2015-09-10 19:08:41 -0700

    #53: Finished some of Elizabeth's recommendations - happyness page, Medium section emphasis, and image carouseling forward. Didn't get to icon accessbility on every page or 'see next project'

--- js/script.js
+++ js/script.js
@@ -1,6 +1,65 @@
 $(document).ready(function() {
-	/******** Portfolio Projects ********/
 
+	/******** Home page ********/
+
+	var linkPort = $("#link-portfolio");
+	linkPort.click(function() {
+		var id = $(this).attr('data-href');
+		$('html,body').animate({
+			scrollTop: $(id).offset().top},
+			400);
+	});
+
+	/******** Portfolio projects ********/
+
+	// Scroll animation from table of contents to section
+	var secTitle = $(".list-roles-ol a");
+	secTitle.click(function() {
+		var id = $(this).attr('data-href');
+		$('html,body').animate({
+			scrollTop: $(id).offset().top},
+			400);
+	});
+
+	// Click on image container to view the next image
+	var imgContainer = $(".image-container");
+
+	imgContainer.click(function() {
+		// Replace the main image according to the selected thumbnail's index
+		var currThumb = $(this).closest(".row").find(".thumbnail-selected");
+		var currIdx = currThumb.index();
+
+		// determine correct image menu to get the current image base
+		var imgMenu = $(this).closest(".row").find(".image-menu");
+		var allMenuImages = imgMenu.children('img');
+		var allImagesSize = allMenuImages.length;
+
+		// Get the next image index
+		var nextImgIdx, nextImgThumb;
+		// If the current index is the last image, the next image is index 0
+		if (currIdx == allImagesSize - 1) {
+			nextImgIdx = 0;
+			nextImgThumb = $(allMenuImages[nextImgIdx]);
+		} else {
+			nextImgIdx = currIdx + 1;
+			nextImgThumb = $(allMenuImages[nextImgIdx]);
+		}
+
+		// Get the image container to switch images
+		var allContainerImages = $(this).children('img');
+		var currImg = $(this).find(".image-placeholder");
+		var nextImg = $(allContainerImages[nextImgIdx]);
+
+		// toggle the selected thumbnail
+		currThumb.removeClass('thumbnail-selected');
+		nextImgThumb.addClass('thumbnail-selected');
+		
+		// fade out the old image and fade in the new image by changing classes
+		nextImg.toggleClass('image-placeholder carousel-image');
+		currImg.toggleClass('image-placeholder carousel-image');
+	});
+
+	// Select thumbnail
 	var selThumbnail = $(".thumbnail");
 
 	selThumbnail.click(function() {
@@ -75,9 +134,9 @@ $(document).ready(function() {
 			scrollName: 'scrollUp', // Element ID
 			scrollDistance: '2000', // Distance from top before showing element (px)
 			scrollSpeed: 300, // Speed back to top (ms)
-			animation: 'fade', // Fade, slide, none
-			animationInSpeed: 200, // Animation in speed (ms)
-			animationOutSpeed: 200, // Animation out speed (ms)
+			animation: 'slide', // Fade, slide, none
+			animationInSpeed: 500, // Animation in speed (ms)
+			animationOutSpeed: 500, // Animation out speed (ms)
 			scrollText: '', // Text for element
 			activeOverlay: false, // Set CSS color to display scrollUp active point, e.g '#00FFFF'
 		});
